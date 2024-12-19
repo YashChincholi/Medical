cb6779a7a25f6a42719ce844591bdff7aa344967
Your Name <you@example.com>
2024-12-19 23:49:52 +0530

    Book section done

--- src/app/_container/Book.jsx
+++ src/app/_container/Book.jsx
@@ -1,9 +1,27 @@
-import React from 'react'
+import React from "react";
 
 function Book() {
   return (
-    <div>Book</div>
-  )
+    <div className="relative mt-20 h-[90vh] w-full flex flex-col items-center justify-center text-center">
+      <img
+        src="/book.svg"
+        alt="background"
+        className="object-cover absolute inset-0 w-full h-full z-0 hidden sm:block "
+      />
+      <img
+        src="/bookr.svg"
+        alt="background"
+        className="object-cover absolute inset-0 w-full h-full z-0 sm:hidden block "
+      />
+      <div className="bg-black bg-opacity-50 w-full h-full inset-0 absolute z-10" />
+      <h1 className="relative z-20 text-white mx-20 sm:px-48 font-medium mb-10 sm:text-5xl text-4xl leading-snug">
+        Ready to restore harmony in your mind, body and spirit?
+      </h1>
+      <button className="relative z-20 px-12 sm:px-8 py-4 font-bold bg-primary text-white rounded-xl">
+        Book a Consultation
+      </button>
+    </div>
+  );
 }
 
-export default Book
+export default Book;
